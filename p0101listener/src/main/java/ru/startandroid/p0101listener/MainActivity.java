0ab51c256ac405ea87c6dcdca0028035b7130838
Tigran <tigranma@gmail.com>
2021-06-08 21:54:57 +0300

    Lesson 9 Button click listener (3 types of listener)||Update

---
+++ p0101listener/src/main/java/ru/startandroid/p0101listener/MainActivity.java
@@ -0,0 +1,45 @@
+package ru.startandroid.p0101listener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Нажата кнопка 1");
+            }
+        });
+
+        button2.setOnClickListener(this);
+    }
+
+    public void clickButton3(View view) {
+        textView.setText("Нажата кнопка 3");
+    }
+
+    @Override
+    public void onClick(View v) {
+        textView.setText("Нажата кнопка 2");
+    }
+}
