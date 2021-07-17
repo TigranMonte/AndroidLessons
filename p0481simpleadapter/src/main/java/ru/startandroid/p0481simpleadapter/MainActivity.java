1d130eb1b859fe1c8d207c7aa8c1fef1ea32f0d9
Tigran <tigranma@gmail.com>
2021-07-17 23:48:14 +0300

    Lesson 48 SimpleAdapter

--- p0481simpleadapter/src/main/java/ru/startandroid/p0481simpleadapter/MainActivity.java
+++ p0481simpleadapter/src/main/java/ru/startandroid/p0481simpleadapter/MainActivity.java
@@ -15,19 +15,16 @@ public class MainActivity extends AppCompatActivity {
     final String ATTRIBUTE_NAME_TEXT = "text";
     final String ATTRIBUTE_NAME_CHECKED = "checked";
     final String ATTRIBUTE_NAME_IMAGE = "image";
-
     ListView lvSimple;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // массивы данных
         String[] texts = {"sometext 1", "sometext 2", "sometext 3", "sometext 4", "sometext 5"};
         boolean[] checked = {true, false, false, true, false};
         int img = R.mipmap.ic_launcher;
-
         // упаковываем данные в понятную для адаптера структуру
         ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(texts.length);
         Map<String, Object> m;
@@ -38,18 +35,15 @@ public class MainActivity extends AppCompatActivity {
             m.put(ATTRIBUTE_NAME_IMAGE, img);
             data.add(m);
         }
-
         // массив имен атрибутов, из которых будут читаться данные
         String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_CHECKED, ATTRIBUTE_NAME_IMAGE};
-
         // массив ID View-компонентов, в которые будут вставляться данные
         int[] to = {R.id.tvText, R.id.cbChecked, R.id.ivImg};
-
         // создаем адаптер
         SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.item, from, to);
-
         // определяем список и присваиваем ему адаптер
         lvSimple = findViewById(R.id.lvSimple);
         lvSimple.setAdapter(adapter);
     }
+
 }
