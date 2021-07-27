ffa10e436fda6e4eac2d48114c6d4e1458604f64
Tigran <tigranma@gmail.com>
2021-07-27 22:32:46 +0300

    Lesson 54 Кастомизация списка, создаем свой адаптер на основе BaseAdapter (Update)

--- p0541customadapter/src/main/java/ru/startandroid/p0541customadapter/BoxAdapter.java
+++ p0541customadapter/src/main/java/ru/startandroid/p0541customadapter/BoxAdapter.java
@@ -8,7 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
