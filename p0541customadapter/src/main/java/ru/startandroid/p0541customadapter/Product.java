be9c68e4f8be071c71927950a7bfcfb3f6a2450f
Tigran <tigranma@gmail.com>
2021-07-27 22:30:52 +0300

    Lesson 53 Кастомизация списка, создаем свой адаптер на основе BaseAdapter

---
+++ p0541customadapter/src/main/java/ru/startandroid/p0541customadapter/Product.java
@@ -0,0 +1,15 @@
+package ru.startandroid.p0541customadapter;
+
+public class Product {
+    String name;
+    int price;
+    int image;
+    boolean box;
+
+    public Product(String _describe, int _price, int _image, boolean _box) {
+        this.name = _describe;
+        this.price = _price;
+        this.image = _image;
+        this.box = _box;
+    }
+}
