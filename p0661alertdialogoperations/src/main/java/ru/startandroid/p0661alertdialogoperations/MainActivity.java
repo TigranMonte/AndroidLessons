b8859b7ffabfd9fc779bb3b3c72a43102c5ab306
Tigran <tigranma@gmail.com>
2021-08-16 17:53:56 +0300

    Lesson 66 Диалоги, обработчики и операции

---
+++ p0661alertdialogoperations/src/main/java/ru/startandroid/p0661alertdialogoperations/MainActivity.java
@@ -0,0 +1,65 @@
+package ru.startandroid.p0661alertdialogoperations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+    final int DIALOG = 1;
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG){
+            Log.d(LOG_TAG, "Create");
+            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+            adb.setTitle("Title");
+            adb.setMessage("Message");
+            adb.setPositiveButton("OK", null);
+            dialog = adb.create();
+
+            // обработчик отображения
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Show");
+                }
+            });
+
+            // обработчик отмены
+            dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Cancel");
+                }
+            });
+
+            // обработчик закрытия
+            dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Dismiss");
+                }
+            });
+            return dialog;
+        }
+        return super.onCreateDialog(id);
+    }
+    public void onClick(View v) {
+        showDialog(DIALOG);
+    }
+}
