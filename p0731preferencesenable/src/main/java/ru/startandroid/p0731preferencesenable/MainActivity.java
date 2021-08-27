5763b9a2b26cdc3181d1f5d20c6745f6bad0cf03
Tigran <tigranma@gmail.com>
2021-08-27 12:09:24 +0300

    Lesson 73 Preferences. Управляем активностью настроек (setEnabled)

---
+++ p0731preferencesenable/src/main/java/ru/startandroid/p0731preferencesenable/MainActivity.java
@@ -0,0 +1,24 @@
+package ru.startandroid.p0731preferencesenable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
