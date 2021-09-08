7cd711641d51e30d37c200bf3ce2a4bb4a28dbbc
Tigran <tigranma@gmail.com>
2021-09-08 18:52:35 +0300

    Lesson 83 Handler. Отложенные сообщения, удаление из очереди, Handler.Callback

---
+++ p0831handlermessagemanage/src/main/java/ru/startandroid/p0831handlermessagemanage/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.startandroid.p0831handlermessagemanage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+    Handler h;
+
+    Handler.Callback hc = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            Log.d(LOG_TAG, "what = " + msg.what);
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       h = new Handler(hc);
+       sendMessage();
+    }
+
+    void sendMessage() {
+        Log.d(LOG_TAG, "send messages");
+        h.sendEmptyMessageDelayed(1, 1000);
+        h.sendEmptyMessageDelayed(2, 2000);
+        h.sendEmptyMessageDelayed(3, 3000);
+        h.sendEmptyMessageDelayed(2, 4000);
+        h.sendEmptyMessageDelayed(5, 5000);
+        h.sendEmptyMessageDelayed(2, 6000);
+        h.sendEmptyMessageDelayed(7, 7000);
+        h.removeMessages(2);
+    }
+}
