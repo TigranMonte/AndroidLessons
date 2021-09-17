c5966d776f7d75cd1f36a87c87c5f060a07056f9
Tigran <tigranma@gmail.com>
2021-09-17 23:57:27 +0300

    Lesson 92 Service. Простой пример

---
+++ p0921servicesimple/src/main/java/ru/startandroid/p0921servicesimple/MyService.java
@@ -0,0 +1,41 @@
+package ru.startandroid.p0921servicesimple;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+
+public class MyService extends Service {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind");
+        return null;
+    }
+
+    void someTask() {
+
+    }
+}
