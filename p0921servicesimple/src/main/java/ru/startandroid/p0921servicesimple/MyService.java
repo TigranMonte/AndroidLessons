58ff354c19043d1645691ca0d8507af645180b1a
Tigran <tigranma@gmail.com>
2021-09-18 15:19:09 +0300

    Lesson 92 Service. Простой пример

--- p0921servicesimple/src/main/java/ru/startandroid/p0921servicesimple/MyService.java
+++ p0921servicesimple/src/main/java/ru/startandroid/p0921servicesimple/MyService.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class MyService extends Service {
 
@@ -36,6 +38,18 @@ public class MyService extends Service {
     }
 
     void someTask() {
-
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i<=5; i++) {
+                    Log.d(LOG_TAG, "i = " + i);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                stopSelf();
+            }
+        }).start();
     }
 }
