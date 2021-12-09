5e209c8563c2fdc0a30e5cd5b74a39dba4c98623
Tigran <tigranma@gmail.com>
2021-12-09 22:40:23 +0300

    Starbuzz coffee (SQLite Database)

--- p0422starbuzz/src/main/java/ru/startandroid/p0422starbuzz/StarbuzzDatabaseHelper.java
+++ p0422starbuzz/src/main/java/ru/startandroid/p0422starbuzz/StarbuzzDatabaseHelper.java
@@ -22,16 +22,10 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS DRINK");
+        db.execSQL("DROP TABLE IF EXISTS FOOD");
+        db.execSQL("DROP TABLE IF EXISTS RESTAURANTS");
         updateMyDatabase(db, oldVersion, newVersion);
-    }
-
-    private static void insertDrink(SQLiteDatabase db, String name, String description,
-                                    int resourceId) {
-        ContentValues drinkValues = new ContentValues();
-        drinkValues.put("NAME", name);
-        drinkValues.put("DESCRIPTION", description);
-        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
-        db.insert("DRINK", null, drinkValues);
     }
 
     private static void insertFood(SQLiteDatabase db, String name, String description,
@@ -41,6 +35,15 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
         foodValues.put("DESCRIPTION", description);
         foodValues.put("RESOURCE_ID", resourceId);
         db.insert("FOOD", null, foodValues);
+    }
+
+    private static void insertDrink(SQLiteDatabase db, String name, String description,
+                                    int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
     }
 
     private static void insertRestaurant(SQLiteDatabase db, String name, String description,
@@ -54,16 +57,15 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE FOOD (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "IMAGE_RESOURCE_ID INTEGER);");
             db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                     + "NAME TEXT, "
                     + "DESCRIPTION TEXT, "
                     + "IMAGE_RESOURCE_ID INTEGER);");
-            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
-                    R.drawable.cappuccino);
-            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
-
-            db.execSQL("CREATE TABLE FOOD (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            db.execSQL("CREATE TABLE RESTAURANTS (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                     + "NAME TEXT, "
                     + "DESCRIPTION TEXT, "
                     + "IMAGE_RESOURCE_ID INTEGER);");
@@ -71,11 +73,10 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
             insertFood(db, "Burger", "Our MegaBurger with cheese and meat", R.drawable.burger);
             insertFood(db, "Barbecue", "Highest quality meat, armenian traditional xorovats",
                     R.drawable.barbeque);
-
-            db.execSQL("CREATE TABLE RESTAURANTS (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + "NAME TEXT, "
-                    + "DESCRIPTION TEXT, "
-                    + "IMAGE_RESOURCE_ID INTEGER);");
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
+                    R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
             insertRestaurant(db, "Starbuzz center", "Arthur Morgan st. 10 Just 2 min " +
                     "from the Valentine metro station", R.drawable.bar2);
             insertRestaurant(db, "Starbuzz park", "St. Denny st. 72, " +
@@ -83,6 +84,7 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
             insertRestaurant(db, "Starbuzz sport", "Strawberry road 35, " +
                     "Just 3 min from the City Stadium", R.drawable.bar1);
         }
+
         if (oldVersion < 2 ) {
             db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
             db.execSQL("ALTER TABLE FOOD ADD COLUMN FAVORITE NUMERIC;");
